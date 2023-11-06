124b449a6e8a5674bb42163774592253e54628e2
sameh issa <s11925968@gmail.com>
2023-11-06 23:36:53 +0200

    project

--- src/compount/pageNotFound/Pagenotfound.jsx
+++ src/compount/pageNotFound/Pagenotfound.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Pagenotfound() {
+export default function PageNotFound() {
   return (
     <div className='d-flex justify-content-center align-items-center vh-100'>
       <h2 className='text-danger '>
