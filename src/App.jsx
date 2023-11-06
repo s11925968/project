ca7adf65ac1f756a119e51b24d35a0f68d41c8fd
sameh issa <s11925968@gmail.com>
2023-11-06 23:45:41 +0200

    project

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './compount/Navbar/Navbar.jsx'
 import Home from './compount/Home/Home.jsx'
-//import PageNotFound from './compount/pageNotFound/PageNotFound.jsx'
+import Page from './compount/pageNotFound/Page.jsx'
 import Create from './compount/Create/Create.jsx'
 import CreateUser from './compount/CreateUser/create/CreateUser.jsx'
 import CreateCompany from './compount/CreateCompany/CreateCompany.jsx'
@@ -13,11 +13,11 @@ export default function App() {
     <Navbar />
     <Routes>
       <Route path="/" element={<Home />}/>
+      <Route path="*" element={<Page />}/>
       <Route path="/Contact/create" element={<Create />}/>
       <Route path="/Contact/create/createuser" element={<CreateUser />}/>
       <Route path="/Contact/create/createcompany" element={<CreateCompany />}/>
       <Route path="/Contact/create/logo" element={<LogoUser />}/>
-
     </Routes>
     </>
   )
--- src/compount/pageNotFound/Page.jsx
+++ src/compount/pageNotFound/Page.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function PageNotFound() {
+export default function Page() {
   return (
     <div className='d-flex justify-content-center align-items-center vh-100'>
       <h2 className='text-danger '>
