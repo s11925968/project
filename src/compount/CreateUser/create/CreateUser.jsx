92cc1c7dc3014c67baeb719f5767bc0a9879e011
sameh issa <s11925968@gmail.com>
2023-11-07 19:42:53 +0200

    project

--- src/compount/CreateUser/create/CreateUser.jsx
+++ src/compount/CreateUser/create/CreateUser.jsx
@@ -1,33 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CreatUser.css'
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 export default function CreateUser() {
+  let [user,setUser]=useState({
+    name:'',
+    email:'',
+    password:'',
+    age:'',
+  });
+  const changeUser=(e)=>{
+    const {name,value}=e.target;
+    setUser({...user,[name]:value});
+    console.log(user);
+  };
+  const sendData=async (e)=>{
+    e.preventDefault();
+    const {data}=await axios.post('http://localhost:2000/auth/singup',user);
+    console.log(data);
+  }
   return (
     <>
       <div className="singup d-flex  vh-80">
         <div className="container signup-form">
           <h2>Sign-Up</h2>
-          <form id="signup-form" encType="multipart/form-data">
-            <label htmlFor="firstName">First Name:</label>
-            <input type="text" id="firstName" name="firstName" required />
-            <label htmlFor="lastName">Last Name:</label>
-            <input type="text" id="lastName" name="lastName" required />
+          <form id="signup-form" onSubmit={sendData} encType="multipart/form-data">
+            <label htmlFor="firstName">Name:</label>
+            <input type="text" id="Name" name="name" onChange={changeUser} required />
             <label htmlFor="age">Age:</label>
-            <input type="number" id="age" name="age" required />
+            <input type="number" id="age" name="age"onChange={changeUser} required />
             <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" required />
+            <input type="email" id="email" name="email"onChange={changeUser} required />
             <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" required />
+            <input type="password" id="password" name="password"onChange={changeUser} required />
             <label htmlFor="confirmPassword">Confirm Password:</label>
-            <input
-              type="password"
-              id="confirmPassword"
-              name="confirmPassword"
-              required
-            />
+            <input type="password" id="confirmPassword" name="confirmPassword"/>
             <span id="passwordError" className="error" />
-            <button type="submit" className="w-100">
-              Sign Up
+            <button type="submit" className="btn btn-primary w-100">
+                Submit
             </button>
           </form>
           <div>
--- src/compount/CreateUser/login/LogoUser.jsx
+++ src/compount/CreateUser/login/LogoUser.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './login.css'
+import axios from 'axios';
 export default function LogoUser() {
+  let [user,setUser]=useState({
+    email:'',
+    password:'',
+  });
+  const changeUser =(e)=>{
+    const {name,value} = e.target;
+    setUser({...user,[name]:value});
+    console.log(user);
+  }
+  const sendData=async(e)=>{
+    e.preventDefault();
+    const {data}=await axios.post('',user);
+  }
   return (
     <>
       <div className="singin">
         <div className='arrea'>
         <h2>Login</h2>
-        <form>
+        <form onSubmit={sendData}>
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" required />
+          <input type="email" id="email" name="email" onChange={changeUser} required />
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" required />
-          <button type="submit">Sign In</button>
+          <input type="password" id="password" name="password"onChange={changeUser} required />
+          <div className='d-flex justify-content-center align-items-center pt-2'>
+          <button type="submit ">Sign In</button>
+          </div>
         </form>
         </div>
       </div>
